7ed3f51c2b1a70d49f162bc3882821e03a69c8b6
kaz <kzzz123@protonmail.com>
2022-04-12 02:15:43 +0400

    update graph url

--- balancer-js/src/lib/constants/config.ts
+++ balancer-js/src/lib/constants/config.ts
@@ -12,22 +12,24 @@ export const BALANCER_NETWORK_CONFIG: Record<Network, BalancerNetworkConfig> = {
             contracts: {
                 // vault: '0x7Cd0C38850A3AaBb773311AAAADA3444D31DCE03',
                 vault: '0xB05E81918C00D5a327c3AF5372b9C07528E9400E',
-                multicall: '0xeB93D9EDDCb7476aF0c415E5a6d429454f85833A',
+                // multicall: '0xeB93D9EDDCb7476aF0c415E5a6d429454f85833A',
+                multicall: '0xB97c1132477464d00CC220A825f8b1f1243f9356',
             },
             tokens: {
                 wrappedNativeAsset:
-                    '0xAeaaf0e2c81Af264101B9129C00F4440cCF0F720',//WASTR
-                lbpRaisingTokens: [
-                    '0x6De33698e9e9b787e09d3Bd7771ef63557E148bb', // DAI
-                    '0x6a2d262D56735DbA19Dd70682B39F6bE9a931D98', // USDC 
-                    '0xAeaaf0e2c81Af264101B9129C00F4440cCF0F720', // WASTR
-                ],
+                    '0xAeaaf0e2c81Af264101B9129C00F4440cCF0F720', //WASTR
+                // lbpRaisingTokens: [
+                //     '0x6De33698e9e9b787e09d3Bd7771ef63557E148bb', // DAI
+                //     '0x6a2d262D56735DbA19Dd70682B39F6bE9a931D98', // USDC 
+                //     '0xAeaaf0e2c81Af264101B9129C00F4440cCF0F720', // WASTR
+                // ],
             },
         },
         urls: {
             subgraph:
                 // 'https://graph-node1.starbank.finance/subgraphs/name/starbank-finance/balancer-v2',
-                'https://graph-node1.starbank.finance/subgraphs/name/starbank-finance/balancer-v2-new',
+                // 'https://graph-node1.starbank.finance/subgraphs/name/starbank-finance/balancer-v2-new',
+                'http://graph.starbank.finance/subgraphs/name/starbank-finance/balancer-v2',
         },
         pools: {
             // bbausd: {
